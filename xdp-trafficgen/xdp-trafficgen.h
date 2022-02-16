3882d73f96ec1b48d0e7684c463a06ef0cbaf7fa
Toke Høiland-Jørgensen <toke@redhat.com>
2022-02-16 17:51:17 +0100

    xdp-trafficgen: Add TCP flow suport
    Idea: Open the TCP connection from userspace, but install an XDP program
    that will intercept the ACK and capture the window parameters. Then, once
    the flow has been established, run an XDP program with prog_test_run that
    generates data packets as fast as it can, up to the rwind of the receiver.
    The XDP program on the interface is kept in place to intercept ACKs and
    update the rwin + ACK seqno.
    
    Status: The basic idea works - connecting, and taking over the transmission
    from XDP, including handling ACKs. However, after a while the receiver
    stops acking packets, and the sender gets stuck at the end of the RWIN.
    
    Signed-off-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- xdp-trafficgen/xdp-trafficgen.h
+++ xdp-trafficgen/xdp-trafficgen.h
@@ -4,6 +4,29 @@
 #define XDP_TRAFFICGEN_H
 
 #include <linux/bpf.h>
+#include <linux/if_ether.h>
+#include <linux/ipv6.h>
+
+struct tcp_flowkey {
+  struct in6_addr src_ip;
+  struct in6_addr dst_ip;
+  __u16 dst_port;
+  __u16 src_port;
+};
+
+#define FLOW_STATE_NEW 1
+#define FLOW_STATE_RUNNING 2
+
+struct tcp_flowstate {
+  __u8 dst_mac[ETH_ALEN];
+  __u8 src_mac[ETH_ALEN];
+  __u32 flow_state;
+  __u32 seq;     /* our last sent seqno */
+  __u32 ack_seq; /* last seqno that got acked */
+  __u32 rcv_seq; /* receiver's seqno (our ACK seq) */
+  __u16 window;
+  __u8 wscale;
+};
 
 struct trafficgen_config {
   int ifindex_out;
@@ -12,7 +35,10 @@ struct trafficgen_config {
 };
 
 struct trafficgen_state {
+  struct tcp_flowkey flow_key;
   __u16 next_port;
 };
+
+
 
 #endif
--- xdp-trafficgen/xdp-trafficgen.kern.c
+++ xdp-trafficgen/xdp-trafficgen.kern.c
@@ -7,11 +7,13 @@
 #include <linux/in.h>
 #include <linux/ipv6.h>
 #include <linux/udp.h>
+#include <linux/tcp.h>
 #include <linux/if_ether.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
 #include <xdp/xdp_stats_kern_user.h>
 #include <xdp/xdp_stats_kern.h>
+#include <xdp/parsing_helpers.h>
 
 char _license[] SEC("license") = "GPL";
 
@@ -45,6 +47,151 @@ int xdp_redirect_update_port(struct xdp_md *ctx)
 	}
 	if (state.next_port++ >= config.port_start + config.port_range - 1)
 		state.next_port = config.port_start;
+
+	action = bpf_redirect(config.ifindex_out, 0);
+out:
+	return xdp_stats_record_action(ctx, action);
+}
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 1);
+	__type(key, struct tcp_flowkey);
+	__type(value, struct tcp_flowstate);
+} flow_state_map SEC(".maps");
+
+static int cmp_ipaddr(struct in6_addr *a_, struct in6_addr *b_)
+{
+	__u8 *a = (void *)a_, *b = (void *)b_;
+	int i;
+
+	for (i = 0; i < sizeof(struct in6_addr); i++) {
+		if (*a > *b)
+			return -1;
+		if (*a < *b)
+			return 1;
+		a++;
+		b++;
+	}
+	return 0;
+}
+
+
+SEC("xdp")
+int xdp_handle_tcp_recv(struct xdp_md *ctx)
+{
+	void *data_end = (void *)(long)ctx->data_end;
+	struct tcp_flowstate *fstate, new_fstate = {};
+	void *data = (void *)(long)ctx->data;
+	struct hdr_cursor nh = { .pos = data };
+	struct tcp_flowkey key = {};
+	int eth_type, ip_type, err;
+	struct ipv6hdr *ipv6hdr;
+	struct tcphdr *tcphdr;
+	int action = XDP_PASS;
+	struct ethhdr *eth;
+	__u8 new_match;
+	int i;
+
+	eth_type = parse_ethhdr(&nh, data_end, &eth);
+	if (eth_type != bpf_htons(ETH_P_IPV6))
+		goto out;
+
+	ip_type = parse_ip6hdr(&nh, data_end, &ipv6hdr);
+	if (ip_type != IPPROTO_TCP)
+		goto out;
+
+	if (parse_tcphdr(&nh, data_end, &tcphdr) < 0)
+		goto out;
+
+	/* swap dst and src for received packet */
+	key.dst_ip = ipv6hdr->saddr;
+	key.dst_port = tcphdr->source;
+
+	new_match = !cmp_ipaddr(&key.dst_ip, &state.flow_key.dst_ip) && key.dst_port == state.flow_key.dst_port;
+
+	key.src_ip = ipv6hdr->daddr;
+	key.src_port = tcphdr->dest;
+
+	fstate = bpf_map_lookup_elem(&flow_state_map, &key);
+	if (!fstate) {
+		if (!new_match)
+			goto out;
+
+		new_fstate.flow_state = FLOW_STATE_NEW;
+		new_fstate.seq = bpf_ntohl(tcphdr->ack_seq);
+		for (i = 0; i < ETH_ALEN; i++) {
+			new_fstate.dst_mac[i] = eth->h_source[i];
+			new_fstate.src_mac[i] = eth->h_dest[i];
+		}
+
+		err = bpf_map_update_elem(&flow_state_map, &key, &new_fstate, BPF_NOEXIST);
+		if (err)
+			goto out;
+
+		fstate = bpf_map_lookup_elem(&flow_state_map, &key);
+		if (!fstate)
+			goto out;
+	}
+
+	bpf_printk("Got state seq %u ack_seq %u new %u seq %u new %u window %u\n",
+		   fstate->seq,
+		   fstate->ack_seq, bpf_ntohl(tcphdr->ack_seq),
+		   fstate->rcv_seq, bpf_ntohl(tcphdr->seq), bpf_htons(tcphdr->window));
+
+	fstate->window = bpf_ntohs(tcphdr->window);
+	fstate->ack_seq = bpf_ntohl(tcphdr->ack_seq);
+	fstate->rcv_seq = bpf_ntohl(tcphdr->seq);
+	if (tcphdr->syn)
+		fstate->rcv_seq++;
+
+	/* If we've taken over the flow management, (after the handshake), drop
+	 * the packet
+	 */
+	if (fstate->flow_state == FLOW_STATE_RUNNING)
+		action = XDP_DROP;
+out:
+	return action;
+}
+
+SEC("xdp")
+int xdp_redirect_send_tcp(struct xdp_md *ctx)
+{
+	void *data_end = (void *)(long)ctx->data_end;
+	void *data = (void *)(long)ctx->data;
+	struct tcp_flowstate *fstate;
+	__u32 cur_seq, cksum_diff;
+	int action = XDP_ABORTED;
+	struct ipv6hdr *ipv6hdr;
+	struct tcphdr *tcphdr;
+	__u16 pkt_len;
+
+	ipv6hdr = data + sizeof(struct ethhdr);
+	tcphdr = data + (sizeof(struct ethhdr) + sizeof(struct ipv6hdr));
+	if (tcphdr + 1 > data_end || ipv6hdr + 1 > data_end)
+		goto out;
+
+	fstate = bpf_map_lookup_elem(&flow_state_map, (const void *)&state.flow_key);
+	if (!fstate)
+		goto out;
+
+	pkt_len = bpf_ntohs(ipv6hdr->payload_len) - sizeof(*tcphdr);
+	if (fstate->seq + pkt_len > fstate->ack_seq + (fstate->window << fstate->wscale)) {
+		/* We caught up to the end up the RWIN, spin until ACKs come
+		 * back opening up the window
+		 */
+		bpf_printk("Dropping: %u + %u > %u + %u\n", fstate->seq, pkt_len, fstate->ack_seq, (fstate->window << fstate->wscale));
+		action = XDP_DROP;
+		goto out;
+	}
+
+	cur_seq = bpf_ntohl(tcphdr->seq);
+	cksum_diff = fstate->seq - cur_seq;
+	if (cksum_diff) {
+		tcphdr->check = bpf_htons(~(~bpf_ntohs(tcphdr->check) + cksum_diff));
+		tcphdr->seq = bpf_htonl(fstate->seq);
+	}
+	fstate->seq += pkt_len;
 
 	action = bpf_redirect(config.ifindex_out, 0);
 out:
